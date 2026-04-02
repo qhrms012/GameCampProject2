d0cd96703b26ad5c2fb0a4a4e3ed5dc7aec69744
boguen <qhrms012@naver.com>
2026-04-02 19:30:32 +0900

    [add] 오브젝트 풀 리팩토링

--- Assets/01.Scripts/Managers/PoolManager.cs
+++ Assets/01.Scripts/Managers/PoolManager.cs
@@ -1,23 +1,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum PoolType
+{
+    PlayerBullet,
+    Enemy,
+    EnemyHead
+}
+
 public class PoolManager : Singleton<PoolManager>
 {
-    Dictionary<string, Queue<GameObject>> poolDict = new Dictionary<string, Queue<GameObject>>();
 
-    public GameObject Get(string key, GameObject prefab)
+    public GameObject[] preFabs;
+
+    List<GameObject>[] pools;
+
+    private void Awake()
     {
+        pools = new List<GameObject>[preFabs.Length];
 
-        if(!poolDict.ContainsKey(key))
-            poolDict.Add(key, new Queue<GameObject>());
-
-        if (poolDict[key].Count > 0)
+        for (int i = 0; i < pools.Length; i++)
         {
-            GameObject obj = poolDict[key].Dequeue();
-            obj.SetActive(true);
-            return obj;
+            pools[i] = new List<GameObject>();
         }
-        return Instantiate(prefab);
+    }
 
+    public GameObject Get(PoolType type)
+    {
+        int index = (int)type;
+        GameObject select = null;
+
+        foreach (GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (select == null)
+        {
+            select = Instantiate(preFabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
+    }
+
+    public void Return(GameObject obj)
+    {
+        obj.SetActive(false);
     }
 }
+
