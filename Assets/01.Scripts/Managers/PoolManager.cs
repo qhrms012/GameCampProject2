b9a2f1dc65868431aa026fe871dc574fee3813d7
boguen <qhrms012@naver.com>
2026-04-02 19:00:30 +0900

    [add] 풀매니저 제네리싱글톤 적용

--- Assets/01.Scripts/Managers/PoolManager.cs
+++ Assets/01.Scripts/Managers/PoolManager.cs
@@ -1,16 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour
+public class PoolManager : Singleton<PoolManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    Dictionary<string, Queue<GameObject>> poolDict = new Dictionary<string, Queue<GameObject>>();
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Get(string key, GameObject prefab)
     {
-        
+
+        if(!poolDict.ContainsKey(key))
+            poolDict.Add(key, new Queue<GameObject>());
+
+        if (poolDict[key].Count > 0)
+        {
+            GameObject obj = poolDict[key].Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        return Instantiate(prefab);
+
     }
 }
