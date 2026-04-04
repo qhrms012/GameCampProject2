7a66ed1d072b6afd8fad91f514a872adc8b42c91
boguen <qhrms012@naver.com>
2026-04-04 21:20:24 +0900

    [add] enemy스포너 gamemanager로 옮기기 및 ui에셋 추가

--- Assets/01.Scripts/Managers/GameManager.cs
+++ Assets/01.Scripts/Managers/GameManager.cs
@@ -1,15 +1,54 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    EnemySpawner enemySpawner;
+
+
+    [SerializeField]
+    float spawnRate = 1f;
+    float timer;
+    [SerializeField]
+    int bodyCount;
+
+
+    [SerializeField]
+    Transform[] waypoints;
 
     private void Awake()
     {
-        enemySpawner = FindObjectOfType<EnemySpawner>();
+
     }
     private void Start()
     {
-        enemySpawner.SpawnEnemy();
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        StartCoroutine(SpawnChain());
+    }
+
+    IEnumerator SpawnChain()
+    {
+
+        Enemy head = PoolManager.Instance.Get(PoolType.EnemyHead).GetComponent<Enemy>();
+        head.transform.position = waypoints[0].position;
+        head.SetPath(waypoints);
+
+        Enemy prev = head;
+
+        for (int i = 0; i < bodyCount; i++)
+        {
+            yield return new WaitForSeconds(0.2f); //µô·¹ÀÌ
+
+            Enemy body = PoolManager.Instance.Get(PoolType.Enemy).GetComponent<Enemy>();
+            body.transform.position = prev.transform.position - Vector3.down * 2f; // À§Ä¡ ¸ÂÃçÁÖ±â
+            body.SetTarget(prev.transform);
+
+            body.prev = prev;
+            prev.next = body;
+            prev = body;
+        }
     }
 }
